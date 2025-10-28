4246cb977cdaacbe1d1409ffb0595988d0c547b4
Renato416 <re.rojasc@duocuc.cl>
2025-10-28 03:34:17 -0300

    commit bootstrap

--- tienda-react/src/main.tsx
+++ tienda-react/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HomeAdmin from "./pages/administrador/HomeAdmin"; // Ajusta la ruta si cambia
 import "./assets/CSS/administrador/home_admin.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
